7e8489a48e4bc114a9fc33cf3634898072b73fe1
chungpd <chung6a8m@yahoo.com>
2026-08-07 17:50:11 +0700

    Use RepoDb attributes for CounterItem sample (#12)
    * Use RepoDb attributes for CounterItem
    
    * Use RepoDb entity operations for counter repository
    
    * Document RepoDb attribute mapping sample
    
    * Fix RepoDb ordering compile error
    
    * Fix RepoDb QueryAll orderBy collection

--- samples/NetUnitOfWorkManager.Sample.RepoDb.Net472/Repositories/RepoDbCounterRepository.cs
+++ samples/NetUnitOfWorkManager.Sample.RepoDb.Net472/Repositories/RepoDbCounterRepository.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using NetUnitOfWorkManager.Sample.RepoDb.Net472.Infrastructure;
 using NetUnitOfWorkManager.Sample.RepoDb.Net472.Models;
 using RepoDb;
 
@@ -20,9 +19,11 @@ namespace NetUnitOfWorkManager.Sample.RepoDb.Net472.Repositories
         {
             global::NetUnitOfWorkManager.UnitOfWorkDbSession db = _unitOfWorkManager.Current.Db;
 
-            db.Connection.ExecuteNonQuery(
-                "INSERT INTO " + SqlServerSampleDatabase.TableName + " ([Value]) VALUES (@Value);",
-                new { Value = value },
+            db.Connection.Insert<CounterItem, long>(
+                new CounterItem
+                {
+                    Value = value
+                },
                 transaction: db.Transaction);
         }
 
@@ -30,8 +31,11 @@ namespace NetUnitOfWorkManager.Sample.RepoDb.Net472.Repositories
         {
             global::NetUnitOfWorkManager.UnitOfWorkDbSession db = _unitOfWorkManager.Current.Db;
 
-            return db.Connection.ExecuteQuery<CounterItem>(
-                    "SELECT [Id], [Value] FROM " + SqlServerSampleDatabase.TableName + " ORDER BY [Id];",
+            return db.Connection.QueryAll<CounterItem>(
+                    orderBy: new[]
+                    {
+                        OrderField.Ascending<CounterItem>(item => item.Id)
+                    },
                     transaction: db.Transaction)
                 .ToArray();
         }

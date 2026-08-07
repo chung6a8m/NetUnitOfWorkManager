ce46c7351a57ee5b5d6d44e40c05365361d34739
chungpd <chung6a8m@yahoo.com>
2026-08-07 15:19:07 +0700

    Add RepoDb sample repository contract

---
+++ samples/NetUnitOfWorkManager.Sample.RepoDb.Net472/Repositories/ICounterRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using NetUnitOfWorkManager.Sample.RepoDb.Net472.Models;
+
+namespace NetUnitOfWorkManager.Sample.RepoDb.Net472.Repositories
+{
+    public interface ICounterRepository
+    {
+        void Insert(int value);
+
+        IReadOnlyList<CounterItem> List();
+    }
+}

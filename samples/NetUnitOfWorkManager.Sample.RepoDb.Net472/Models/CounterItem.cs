e314104cfbc3d111067dee09a2b50aa9c028c7a5
chungpd <chung6a8m@yahoo.com>
2026-08-07 15:19:00 +0700

    Add RepoDb sample counter model

---
+++ samples/NetUnitOfWorkManager.Sample.RepoDb.Net472/Models/CounterItem.cs
@@ -0,0 +1,9 @@
+﻿namespace NetUnitOfWorkManager.Sample.RepoDb.Net472.Models
+{
+    public sealed class CounterItem
+    {
+        public long Id { get; set; }
+
+        public int Value { get; set; }
+    }
+}

ef2e47a731c44e93cb42d79e0d37d1ab865b6dbe
chungpd <chung6a8m@yahoo.com>
2026-08-07 15:19:57 +0700

    Add RepoDb transaction scenario service

---
+++ samples/NetUnitOfWorkManager.Sample.RepoDb.Net472/Services/CounterApplicationService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using NetUnitOfWorkManager.Sample.RepoDb.Net472.Models;
+using NetUnitOfWorkManager.Sample.RepoDb.Net472.Repositories;
+
+namespace NetUnitOfWorkManager.Sample.RepoDb.Net472.Services
+{
+    public sealed class CounterApplicationService
+    {
+        private static readonly global::NetUnitOfWorkManager.UnitOfWorkOptions Options =
+            new global::NetUnitOfWorkManager.UnitOfWorkOptions(IsolationLevel.Serializable);
+
+        private readonly global::NetUnitOfWorkManager.IUnitOfWorkManager _unitOfWorkManager;
+        private readonly ICounterRepository _counterRepository;
+        private readonly NestedCounterService _nestedCounterService;
+
+        public CounterApplicationService(
+            global::NetUnitOfWorkManager.IUnitOfWorkManager unitOfWorkManager,
+            ICounterRepository counterRepository,
+            NestedCounterService nestedCounterService)
+        {
+            _unitOfWorkManager = unitOfWorkManager;
+            _counterRepository = counterRepository;
+            _nestedCounterService = nestedCounterService;
+        }
+
+        public IReadOnlyList<CounterItem> List()
+        {
+            using (global::NetUnitOfWorkManager.IUnitOfWorkScope scope =
+                _unitOfWorkManager.Begin(Options))
+            {
+                IReadOnlyList<CounterItem> items = _counterRepository.List();
+                scope.Complete();
+                return items;
+            }
+        }
+
+        public void CommitPair(int outerValue, int innerValue)
+        {
+            using (global::NetUnitOfWorkManager.IUnitOfWorkScope scope =
+                _unitOfWorkManager.Begin(Options))
+            {
+                _counterRepository.Insert(outerValue);
+                _nestedCounterService.InsertAndComplete(innerValue, Options);
+                scope.Complete();
+            }
+        }
+
+        public void RollbackPair(int outerValue, int innerValue)
+        {
+            using (global::NetUnitOfWorkManager.IUnitOfWorkScope scope =
+                _unitOfWorkManager.Begin(Options))
+            {
+                _counterRepository.Insert(outerValue);
+                _nestedCounterService.InsertWithoutCompleting(innerValue, Options);
+                scope.Complete();
+            }
+        }
+    }
+}

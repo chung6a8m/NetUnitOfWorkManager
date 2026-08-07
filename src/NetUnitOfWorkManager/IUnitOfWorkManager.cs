67ba4f9790576c1f493af55c30bff7e0a697172b
chungpd <chung6a8m@yahoo.com>
2026-08-07 11:19:11 +0700

    feat: define P02 minimal public contracts

---
+++ src/NetUnitOfWorkManager/IUnitOfWorkManager.cs
@@ -0,0 +1,25 @@
+﻿namespace NetUnitOfWorkManager
+{
+    /// <summary>
+    /// Defines the ambient Unit of Work manager contract.
+    /// </summary>
+    public interface IUnitOfWorkManager
+    {
+        /// <summary>
+        /// Gets a value indicating whether this manager has a current ambient Unit of Work.
+        /// </summary>
+        bool HasCurrent { get; }
+
+        /// <summary>
+        /// Gets the current ambient Unit of Work context.
+        /// </summary>
+        IUnitOfWorkContext Current { get; }
+
+        /// <summary>
+        /// Begins a Unit of Work scope.
+        /// </summary>
+        /// <param name="options">Optional transaction options.</param>
+        /// <returns>A scope token representing the started Unit of Work scope.</returns>
+        IUnitOfWorkScope Begin(UnitOfWorkOptions? options = null);
+    }
+}
---
+++ src/NetUnitOfWorkManager/UnitOfWorkStateException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NetUnitOfWorkManager
+{
+    /// <summary>
+    /// Represents invalid Unit of Work lifecycle or scope usage.
+    /// </summary>
+    public sealed class UnitOfWorkStateException : InvalidOperationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWorkStateException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the invalid Unit of Work state.</param>
+        public UnitOfWorkStateException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWorkStateException"/> class with an inner exception.
+        /// </summary>
+        /// <param name="message">The message that describes the invalid Unit of Work state.</param>
+        /// <param name="innerException">The exception that caused the current exception.</param>
+        public UnitOfWorkStateException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}

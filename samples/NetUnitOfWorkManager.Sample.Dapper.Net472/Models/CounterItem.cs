db527cffb62115e6d74aaaa1f12bc75db43ba0ca
chungpd <chung6a8m@yahoo.com>
2026-08-08 08:29:12 +0700

    feat: add Dapper counter model

---
+++ samples/NetUnitOfWorkManager.Sample.Dapper.Net472/Models/CounterItem.cs
@@ -0,0 +1,9 @@
+﻿namespace NetUnitOfWorkManager.Sample.Dapper.Net472.Models
+{
+    public sealed class CounterItem
+    {
+        public long Id { get; set; }
+
+        public int Value { get; set; }
+    }
+}

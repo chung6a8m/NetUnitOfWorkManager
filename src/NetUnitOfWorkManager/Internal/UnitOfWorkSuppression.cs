bd24f427c4305f247ff6b1ef25ab681e3821d37f
chungpd <chung6a8m@yahoo.com>
2026-08-08 07:35:46 +0700

    feat: add suppression token

---
+++ src/NetUnitOfWorkManager/Internal/UnitOfWorkSuppression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NetUnitOfWorkManager.Internal
+{
+    internal sealed class UnitOfWorkSuppression : IDisposable
+    {
+        private readonly object _sync = new object();
+        private readonly UnitOfWorkManager _manager;
+        private readonly AmbientUnitOfWorkFrame _boundary;
+        private bool _disposed;
+
+        internal UnitOfWorkSuppression(
+            UnitOfWorkManager manager,
+            AmbientUnitOfWorkFrame boundary)
+        {
+            _manager = manager ?? throw new ArgumentNullException(nameof(manager));
+            _boundary = boundary ?? throw new ArgumentNullException(nameof(boundary));
+        }
+
+        public void Dispose()
+        {
+            lock (_sync)
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+
+                _manager.RestoreSuppression(_boundary);
+                _disposed = true;
+            }
+        }
+    }
+}
